0729b3def14af071afb50ac913e0a76eaec5cc3e
Sahil <leviack629@gmail.com>
2022-12-03 16:55:55 +0530

    latest modifications

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import { AiFillTwitterCircle } from 'react-icons/ai'
+
 
 const Footer = () => {
   return (
     <div className='bg-black text-center h-20'>
         <div className=''>
-            {/* <p className='text-white pt-7'>Made with &hearts; by Sahil Malik</p> */}
+    
             <p className='text-white pt-7'>&copy; 2022 Sahil Malik</p>
             
         </div>
